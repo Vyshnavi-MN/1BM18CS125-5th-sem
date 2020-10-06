74da3cba022cbf01e5715dd638a4100276b0f307
Vyshnavi <53755084+Vyshnavi2000@users.noreply.github.com>
2020-10-06 14:34:49 +0530

    Update biggest.sh

--- USP/biggest.sh
+++ USP/biggest.sh
@@ -7,10 +7,10 @@ read num3
 
 if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
 then
-    echo $num1
+    echo "$num1 is the largest"
 elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
 then
-    echo $num2
+    echo "$num2 is the largest"
 else
-    echo $num3
+    echo "$num3 is the largest"
 fi
