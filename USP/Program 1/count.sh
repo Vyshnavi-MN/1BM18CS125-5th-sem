0b6fb0353a145581f6c2627770283dc2bd1cdbb8
Vyshnavi <53755084+Vyshnavi2000@users.noreply.github.com>
2020-10-13 15:28:33 +0530

    Update count.sh

--- USP/Program 1/count.sh
+++ USP/Program 1/count.sh
@@ -1,1 +1,1 @@
-str = "My meeme said his meeme is a stupid meeme.\n My meeme also said that his meeme is a dumbass."
+str = "Suheldev - The king who saved India is a legendary book written by Amish"
