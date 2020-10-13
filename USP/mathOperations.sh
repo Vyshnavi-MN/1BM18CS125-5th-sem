0d7dce16788276d55699a04da7af69e04771cfe4
Vyshnavi <53755084+Vyshnavi2000@users.noreply.github.com>
2020-10-13 15:25:14 +0530

    Delete mathOperations.sh

--- USP/mathOperations.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter Two numbers--->\nEnter First number: "
-read number1
-echo "Enter Second number: "
-read number2
-add=`expr $number1 + $number2`
-sub=`expr $number1 - $number2`
-div=`expr $number1 / $number2`
-mult=`expr $number1 \* $number2`
-echo "Sum: $add\nDifference: $sub\nDivision: $div\nMultiplication: $mult"
