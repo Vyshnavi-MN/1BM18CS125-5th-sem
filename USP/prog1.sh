e3289347faa21e03bde8f997515bc8fcca9f2d77
Vyshnavi <53755084+Vyshnavi2000@users.noreply.github.com>
2020-10-13 15:25:41 +0530

    Delete prog1.sh

--- USP/prog1.sh
+++
@@ -1,6 +0,0 @@
-echo "Enter basic salary: "
-read basicSalary
-da=`expr $basicSalary + $basicSalary \* 20 / 100`
-hra=`expr $basicSalary + $BasicSalary \* 30 / 100`
-grossSalary=`expr $da + $hra`
-echo "Gross Salary = $grossSalary"
