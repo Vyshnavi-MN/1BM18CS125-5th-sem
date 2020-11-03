fd48d8c0acc4ca5076dddc339ff9141c35df1081
Vyshnavi <53755084+Vyshnavi2000@users.noreply.github.com>
2020-11-03 15:09:40 +0530

    Add files via upload

---
+++ USP/Program 5/checkVowel.sh
@@ -0,0 +1,16 @@
+echo "Enter any character: "
+read ch
+case $ch in
+"a") echo "It is a vowel.";;
+"e") echo "It is a vowel.";;
+"i") echo "It is a vowel.";;
+"o") echo "It is a vowel.";;
+"u") echo "It is a vowel.";;
+"A") echo "It is a vowel.";;
+"E") echo "It is a vowel.";;
+"I") echo "It is a vowel.";;
+"O") echo "It is a vowel.";;
+"U") echo "It is a vowel.";;
+*) echo "It is not a vowel."
+esac
+
